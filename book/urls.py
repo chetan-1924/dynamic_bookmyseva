d72b4cd9bae654e728e334c33caaacd8e1ef999b
chetan-1924 <chetanreddy1924@gmail.com>
2021-04-02 22:50:54 +0530

    Made services page dynamic

--- book/urls.py
+++ book/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about_us_page, name="about"),
     path('contact/', views.contact, name="contact"),
-    path('pricing/', views.pricing, name="pricing")
+    path('pricing/', views.pricing, name="pricing"),
+    path('service/', views.service, name="service")
 ]
 # urlpatterns = [path("about/", views.about, name="about")]
 # urlpatterns = [path("/contact", views.contact, name="contact")]
--- book/views.py
+++ book/views.py
@@ -8,5 +8,7 @@ def pricing(request):
     return render(request, 'pricing.html')
 def contact(request):
     return render(request, 'contact.html')
+def service(request):
+    return render(request, 'service.html')
 # def contact(request):
 #     return render(request, 'contact.html')
