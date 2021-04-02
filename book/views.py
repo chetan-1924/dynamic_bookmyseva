d94b383f337ce526b1abeec4e030b0ee205165e3
chetan-1924 <chetanreddy1924@gmail.com>
2021-04-02 22:45:24 +0530

    Made hyperlinks of contact and register page dynamic

--- book/views.py
+++ book/views.py
@@ -4,5 +4,9 @@ def index(request):
     return render(request, 'index.html')
 def about_us_page(request):
     return render(request, 'about.html')
+def pricing(request):
+    return render(request, 'pricing.html')
+def contact(request):
+    return render(request, 'contact.html')
 # def contact(request):
 #     return render(request, 'contact.html')
